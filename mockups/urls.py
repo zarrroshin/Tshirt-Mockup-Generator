34e63d39b0d7e5ecbce773494882e7223ca97253
zahra <zahraroshani973@gmail.com>
2025-11-03 23:13:36 +0330

    fixing urls

--- mockups/urls.py
+++ mockups/urls.py
@@ -3,7 +3,7 @@ from .views import hello
 from .views import generate_mockup_view,task_status_view,MockupListView
 
 urlpatterns = [
-    path('hello/', hello),
+    path('hello/', hello), #for testing 
     path('mockups/generate/', generate_mockup_view),
     path('tasks/<str:task_id>/',task_status_view ),
     path('mockups/', MockupListView.as_view()), 
